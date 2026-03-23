6a8da47850ff1eef5d8ba169311bf6f26358655a
Gautier <Hybanos@orange.fr>
2026-03-23 23:50:51 +0100

    vec3 operator overloading

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -3,7 +3,41 @@ struct vec3 {
     double y;
     double z;
 
-    inline double dist_squared(vec3 o) {
-        return (x - o.x) * (x - o.x) + (y - o.y) * (y - o.y) + (z - o.z) * (z - o.z);
+    // scalar
+    inline vec3 operator+(double d) {
+        return vec3{x + d, y + d, z + d};
+    }
+
+    inline vec3 operator-(double d) {
+        return vec3{x - d, y - d, z - d};
+    }
+    
+    inline vec3 operator*(double d) {
+        return vec3{x * d, y * d, z * d};
+    }
+
+    inline vec3 operator/(double d) {
+        return vec3{x / d, y / d, z / d};
+    }
+
+    // vector
+    inline vec3 operator+(vec3 v) {
+        return vec3{x + v.x, y + v.y, z + v.z};
+    }
+
+    inline vec3 operator-(vec3 v) {
+        return vec3{x - v.x, y - v.y, z - v.z};
+    }
+
+    inline vec3 operator*(vec3 v) {
+        return vec3{x * v.x, y * v.y, z * v.z};
+    }
+
+    inline vec3 operator/(vec3 v) {
+        return vec3{x / v.x, y / v.y, z / v.z};
+    }
+
+    inline double dist_squared(vec3 v) {
+        return (x - v.x) * (x - v.x) + (y - v.y) * (y - v.y) + (z - v.z) * (z - v.z);
     }
 };
