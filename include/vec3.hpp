1b11731ece4ff7c69568743648c4c01ec8a6d78b
Gautier <hybanos@orange.fr>
2026-03-24 10:58:47 +0100

    vec3 operator overloading

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -3,6 +3,38 @@ struct vec3 {
     double y;
     double z;
 
+    inline vec3 operator+(vec3 v) {
+        return vec3{x + v.x, y + v.y, z + v.z};
+    }
+
+    inline vec3 operator-(vec3 v) {
+        return vec3{x - v.x, y - v.y, z - v.z};
+    }
+
+    inline vec3 operator*(vec3 v) {
+        return vec3{x * v.x, y * v.y, z * v.z};
+    }
+
+    inline vec3 operator/(vec3 v) {
+        return vec3{x / v.x, y / v.y, z / v.z};
+    }
+
+    inline vec3 operator+(double d) {
+        return vec3{x + d, y + d, z + d};
+    }
+
+    inline vec3 operator-(double d) {
+        return vec3{x - d, y - d, z - d};
+    }
+
+    inline vec3 operator*(double d) {
+        return vec3{x * d, y * d, z * d};
+    }
+
+    inline vec3 operator/(double d) {
+        return vec3{x / d, y / d, z / d};
+    }
+
     inline double dist_squared(vec3 o) {
         return (x - o.x) * (x - o.x) + (y - o.y) * (y - o.y) + (z - o.z) * (z - o.z);
     }
--- src/system.cpp
+++ src/system.cpp
@@ -12,32 +12,18 @@ void System::step() {
     double dist_squared = p1.dist_squared(p2);
     double dist = std::sqrt(dist_squared);
 
-    double dx = p2.x - p1.x;
-    double dy = p2.y - p1.y;
-    double dz = p2.z - p1.z;
+    vec3 dx = p2 - p1;
 
     // forces
-    double dvx = gamma * m1 * m2 * dx / (dist_squared * dist);
-    double dvy = gamma * m1 * m2 * dy / (dist_squared * dist);
-    double dvz = gamma * m1 * m2 * dz / (dist_squared * dist);
+    vec3 dv = dx * gamma * m1 * m2 / (dist_squared * dist);
 
     // speeds
-    v1.x += dvx * dt;
-    v1.y += dvy * dt;
-    v1.z += dvz * dt;
-
-    v2.x -= dvx * dt;
-    v2.y -= dvy * dt;
-    v2.z -= dvz * dt;
+    v1 = v1 + dv * dt;
+    v2 = v2 - dv * dt;
 
     // pos
-    p1.x += v1.x * dt / m1;
-    p1.y += v1.y * dt / m1;
-    p1.z += v1.z * dt / m1;
-
-    p2.x += v2.x * dt / m2;
-    p2.y += v2.y * dt / m2;
-    p2.z += v2.z * dt / m2;
+    p1 = p1 + v1 * dt / m1;
+    p2 = p2 + v2 * dt / m2;
 
     print();
 }
